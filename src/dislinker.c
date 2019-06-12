0349bd9fc2135f536b5287893e3855d2a7faeb11
hstiv <satmak335@gmail.com>
2019-06-12 18:07:00 +0300

    last_breath

--- src/dislinker.c
+++ src/dislinker.c
@@ -4,43 +4,31 @@
 
 #include "lem_in.h"
 
-static void 		link_mark(int nb, t_room **adj, int nb1)
+static void 		switch_them(int nb, int nb1, t_room **adj)
 {
 	t_room			*room;
 
-	room = adj[nb];
-	room->link_count -= 1;
 	room = adj[nb]->next;
 	while (room->self != adj[nb1] && room->next)
 		room = room->next;
-	room->visited = 1;
-	room->self->link_count -= 1;
-	room = room->self->next;
-	while (room->self != adj[nb] && room->next)
-		room = room->next;
-	room->visited = 1;
+	if (room && room->visited == 0)
+		room->visited = 1;
+	else if (room && room->visited == 1)
+		room->visited = 0;
 }
 
-void				dislinker(t_lem *lem, t_path *path)
+void				switch_links(t_path *path, t_lem *lem)
 {
 	t_room			*room;
 	t_room			*tmp;
 	t_room			**adj;
 
 	adj = lem->adj;
-	room = path->end->prev;
-	tmp = room;
-	while (tmp->link_count <= 2 && adj[tmp->nb] != lem->begin)
-		tmp = tmp->prev;
-	if (tmp->link_count > 2 && adj[tmp->nb] != lem->begin)
-		link_mark(tmp->nb, adj, tmp->next->nb);
-	else
+	room = path->start;
+	while (room->next)
 	{
-		tmp = tmp->next;
-		while (tmp->next)
-		{
-			adj[tmp->nb]->visited = 1;
-			tmp = tmp->next;
-		}
+		tmp = room->next;
+		switch_them(room->nb, tmp->nb, adj);
+		room = room->next;
 	}
 }
